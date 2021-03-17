f95e77f40de518edfa18250ddbe057858d7c1193
Gabriel Robinson <gabrielrobinson320@gmail.com>
2021-03-17 14:56:15 -0600

    added load and save methods for pages

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,6 +1,24 @@
 package main
 
-type page struct {
+import "io/ioutil"
+
+type Page struct {
 	Title string
 	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	// 0600 gives read write priviledges
+	// wripte the body of page fo file with the specified filename
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
